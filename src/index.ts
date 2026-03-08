128b0bae7bc0ece69e83452357a36a531eb3c9e1
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2026-03-08 19:20:42 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,12 @@ import { uploadRoutes } from "./routes/upload.js";
 import cors from "@elysiajs/cors";
 
 const app = new Elysia();
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 connectDB();
 
