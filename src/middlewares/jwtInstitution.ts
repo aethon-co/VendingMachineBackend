b79695707c1ee5601cbac1f93b650a946fd6f011
Anantha Krishnan <jananthak@gmail.com>
2026-03-08 19:51:23 +0530

    fix: extend institution JWT expiry to 7d

--- src/middlewares/jwtInstitution.ts
+++ src/middlewares/jwtInstitution.ts
@@ -4,7 +4,7 @@ import { t } from 'elysia'
 export const jwtMiddlewareInstitution = jwt({
     name: 'jwt_institution',
     secret: process.env.JWT_INSTITUTION_SECRET!,
-    exp: '15m',
+    exp: '7d',
     schema: t.Object({
         id: t.String(),
         mail: t.String(),
